8629f1be94e9441db02348fafd11b6aaa3a7afaa
Stanislav <vstasv05@yandex.ru>
2024-02-29 22:39:43 +0700

    add exceptions for base sorting

--- product_manager/product/signals.py
+++ product_manager/product/signals.py
@@ -10,18 +10,15 @@ def my_m2m_changed_handler(sender, instance, action, **kwargs):
     if action == 'post_add':
         student_products = instance.products.all()
         for product in student_products:
-            min_users = product.min_users
+            # min_users = product.min_users
             max_users = product.max_users
             product_groups = GroupModel.objects.all().filter(product_id = product.id)
             for group in product_groups:
-                print(f'{group.id} [{group.name}]')
                 check = instance.groupmodel_set.filter(product_id=product.id)
-                print(check)
-                print(check.count())
-                print(f'MAX_USERS: {max_users}')
                 if group.students.count() < max_users and check.count() != 1:
-                    print(f'BEFORE: {group.students.count()}')
                     group.students.add(instance)
-                    print(f'AFTER: {group.students.count()}')
-                else:
-                    print('it break')
+                    break
+                elif group == product_groups.last():
+                    print(f'Группы для продукта {product.name} переполнены! Необходимо добавить новую группу или расширить максимальное кол-во человек в группе.')
+                    
+
