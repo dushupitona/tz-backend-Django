c6cfc022ab6e5cfdea2dc44c94b6bb73592f7221
Stanislav <vstasv05@yandex.ru>
2024-03-02 22:07:03 +0700

    add readme

--- product_manager/product/urls.py
+++ product_manager/product/urls.py
@@ -5,5 +5,5 @@ app_name = 'product'
 
 urlpatterns = [
     path('products/', ProductListAPIView.as_view(), name='product_list'),
-    path('product/<int:product_id>/group/<int:group_id>', StudentProductLessonsAPIView.as_view(), name='product_lessons'),
+    path('product/<int:product_id>/group/<int:group_id>/', StudentProductLessonsAPIView.as_view(), name='product_lessons'),
 ]
